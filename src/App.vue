525a016987a5d357ac95d8b251d058effeddce53
Sebastian <sebaseboruko@gmail.com>
2024-09-11 09:35:03 -0700

    V-for

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,52 @@
 <script setup>
 import { ref, computed } from 'vue'
 
-// Definimos las variables reactivas
 const nombre = ref('')
+const apellido = ref('')
 const count = ref(0)
 const num1 = ref(0)
 const num2 = ref(0)
 const resultado = computed(() => num1.value + num2.value)
+const users = ref([]) 
+
+const agregarUsuarios = () => {
+  users.value.push({
+    nombre: nombre.value,
+    apellido: apellido.value
+  })
+  nombre.value = ''
+  apellido.value = ''
+}
 </script>
 
 <template>
   <div id="app">
-    <h1>Mi nombre es: {{ nombre }}</h1>
-    <br>
-    <input type="text" v-model="nombre" placeholder="Escribe tu nombre aquí">
-    <br><br>
     <button @click="count++">
       Count is: {{ count }}
     </button>
+
     <br>
     <input type="number" v-model="num1" placeholder="Escribe el primer número">
     <br><br>
     <input type="number" v-model="num2" placeholder="Escribe el segundo número">
     <br><br>
     <h2>El resultado es: {{ resultado }}</h2>
+    <br>
+    <form @submit.prevent="agregarUsuarios">
+      <input type="text" v-model="nombre" placeholder="Escribe tu nombre">
+      <br>
+      <input type="text" v-model="apellido" placeholder="Escribe tu apellido">
+      <br>
+      <button type="button" @click="agregarUsuarios">Agregar usuario</button>
+    </form>
+    <ul>
+      <li v-for="(user, index) in users" :key="index">
+        {{ user.nombre }} {{ user.apellido }}
+      </li>
+    </ul>
   </div>
 </template>
+
 
 <style>
   button {
@@ -35,5 +56,8 @@ const resultado = computed(() => num1.value + num2.value)
     font-size: 18px;
   }
 </style>
+
+
+
 
 
