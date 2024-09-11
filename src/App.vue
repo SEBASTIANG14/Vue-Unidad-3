7488b014873eecfe6e862bf5358a59cf89abec61
Sebastian <sebaseboruko@gmail.com>
2024-09-10 22:23:58 -0700

    Inicios de vue

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+// Definimos las variables reactivas
+const nombre = ref('')
+const count = ref(0)
+const num1 = ref(0)
+const num2 = ref(0)
+const resultado = computed(() => num1.value + num2.value)
+</script>
+
+<template>
+  <div id="app">
+    <h1>Mi nombre es: {{ nombre }}</h1>
+    <br>
+    <input type="text" v-model="nombre" placeholder="Escribe tu nombre aquí">
+    <br><br>
+    <button @click="count++">
+      Count is: {{ count }}
+    </button>
+    <br>
+    <input type="number" v-model="num1" placeholder="Escribe el primer número">
+    <br><br>
+    <input type="number" v-model="num2" placeholder="Escribe el segundo número">
+    <br><br>
+    <h2>El resultado es: {{ resultado }}</h2>
+  </div>
+</template>
+
+<style>
+  button {
+    width: 200px;
+    height: 50px;
+    padding: 10px 20px; 
+    font-size: 18px;
+  }
+</style>
+
+
