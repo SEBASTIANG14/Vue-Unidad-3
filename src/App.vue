c9b4ad3c8dcfb013fd1745fd6ea6aba19f2d2ad6
Sebastian <sebaseboruko@gmail.com>
2024-09-18 00:29:23 -0700

    Login y tabla

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
-  <h1>Login Perrón</h1>
-  <form @submit.prevent="onSubmit" v-if="!access">
-    <fieldset>
-      <label>Correo</label>
-      <input v-model="email" type="email" placeholder="Escribe tu correo aquí" required />
-    </fieldset>
-    <fieldset>
-      <label>Contraseña</label>
-      <input v-model="password" type="password" placeholder="Escribe tu contraseña aquí" required />
-    </fieldset>
-    <button type="submit">Iniciar sesión</button>
-  </form>
+  <div>
+    <h1>Login Perrón</h1>
+    <form @submit.prevent="onSubmit" v-if="!access">
+      <fieldset>
+        <label>Correo</label>
+        <input v-model="email" type="email" placeholder="Escribe tu correo aquí" required />
+      </fieldset>
+      <fieldset>
+        <label>Contraseña</label>
+        <input v-model="password" type="password" placeholder="Escribe tu contraseña aquí" required />
+      </fieldset>
+      <button type="submit">Iniciar sesión</button>
+    </form>
 
-  <div class="bienvenida" v-if="access">
-    <h1>ola k ace</h1>
+    <div class="bienvenida" v-if="access">
+      <h1>Hola, bienvenido!</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in users" :key="user.id">
+            <td>{{ user.name }}</td>
+            <td>{{ user.username }}</td>
+            <td>{{ user.phone }}</td>
+            <td>
+              <button @click="editUser(user)">Editar</button>
+              <button @click="deleteUser(user.id)">Borrar</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -25,6 +48,7 @@ export default {
     const email = ref('')
     const password = ref('')
     const access = ref(false)
+    const users = ref([])
 
     const onSubmit = () => {
       fetch('/users.json')
@@ -36,6 +60,7 @@ export default {
 
         if (user) {
           access.value = true;
+          users.value = data; 
           alert('Login exitoso, bienvenido!');
         } else {
           alert('Correo o contraseña incorrectos');
@@ -45,16 +70,45 @@ export default {
         alert('Ocurrió un error: ' + e.message);
       });
     }
+
+    const deleteUser = (id) => {
+      users.value = users.value.filter(user => user.id !== id);
+      alert('Usuario eliminado.');
+    }
+
+    const editUser = (user) => {
+      
+    }
+
     return {
       email,
       password,
       access,
       onSubmit,
+      users,
+      deleteUser,
+      editUser,
     }
   },
 }
 </script>
 
 <style>
- 
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+table, th, td {
+  border: 1px solid black;
+}
+th, td {
+  padding: 8px;
+  text-align: left;
+}
+th {
+  background-color: #f2f2f2;
+}
+button {
+  margin-right: 5px;
+}
 </style>
